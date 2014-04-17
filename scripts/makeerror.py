84d3b654fa3f545d2da4ba529adcc03d8e32eb81
Katie A. Cunningham <katie.cunningham@coxinc.com>
2014-04-17 15:15:36 -0400

    Done with error types

--- scripts/makeerror.py
+++ scripts/makeerror.py
@@ -1,9 +1,18 @@
-def get_item():
-    item = raw_input("Give me an item (q to quit): ")
-    return item
+def get_name():
+	name = raw_input("Name: ")
+	return name
 
-def get_list():
-    my_list = []
-    item = "":
-    while item != 'q':
-        
+def get_age():
+	age = input("Age: ")
+	return age
+
+def print_stuff(name, age):
+	print "Hi, " + name + ", who is " + age + "."
+
+def main():
+	name = get_name()
+	age = get_age()
+	print_stuff(name, age)    
+
+if __name__ == '__main__':
+	main()
